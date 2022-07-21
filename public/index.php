28d666ab5e94726221141f228617fb6c526f0736
Posashkov AS <alexpos@list.ru>
2022-07-21 23:30:38 +0200

    Added form for create a new user. Added user fields validator. Search form refactoring

--- public/index.php
+++ public/index.php
@@ -4,8 +4,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use App\Validator;
 
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$users = [];
+if (($usersFromJson = file_get_contents(__DIR__ . '/../users.json')) !== false) {
+    $users = json_decode($usersFromJson, true);
+}
 
 
 $container = new Container();
@@ -23,21 +27,49 @@ $app->get('/', function ($request, $response) {
 
 $app->get('/users', function ($request, $response, $args) use ($users) {
     $term = htmlspecialchars($request->getQueryParam('term'));
-    $filteredUsers = array_filter($users, fn ($user) => str_contains($user, $term));
+    $filteredUsers = array_filter($users, fn ($user) => str_contains(strtolower($user['nickname']), strtolower($term)));
     $params['users'] = $filteredUsers;
     $params['term'] = $term;
     
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-//    return $response->write('POST /users');
-    return $response->withStatus(302);
+$app->post('/users', function ($request, $response) use ($users) {
+    $parsedUser = $request->getParsedBodyParam('user');
+    $user = [
+        'nickname' => htmlspecialchars($parsedUser['nickname']),
+        'email' => htmlspecialchars($parsedUser['email']),
+        'id' => rand(1, 1000000),
+    ];
+
+    $errors = (new Validator)->validate($user);
+
+    if (count($errors) === 0) {
+        $users[] = $user;
+        file_put_contents(__DIR__ . '/../users.json', json_encode($users));
+
+        return $response->withRedirect('/users', 302);        
+    }
+    
+    $params = [
+        'errors' => $errors,
+        'user' => $user,
+    ];
+    return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
+$app->get('/users/{id:[0-9]+}', function ($request, $response, $args) {
     $params = ['id' => htmlspecialchars($args['id']), 'nickname' => 'user-' . htmlspecialchars($args['id'])];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['nickname' => '', 'email' => ''],
+        'error' => [],
+    ];
+    
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
