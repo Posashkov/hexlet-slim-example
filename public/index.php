6d90ca4f3bd884fbcc1f07bc1d0dbe11a38f9019
Posashkov AS <alexpos@list.ru>
2022-07-22 22:22:08 +0200

    Made all routes named

--- public/index.php
+++ public/index.php
@@ -20,21 +20,30 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->get('/', function ($request, $response) {
    $response->getBody()->write('Welcome to Slim!');
    return $response; 
-});
+})->setName('index');
 
-$app->get('/users', function ($request, $response, $args) use ($users) {
+$app->get('/users', function ($request, $response, $args) use ($users, $router) {
     $term = htmlspecialchars($request->getQueryParam('term'));
     $filteredUsers = array_filter($users, fn ($user) => str_contains(strtolower($user['nickname']), strtolower($term)));
+    
+    $filteredUsers = array_map(function ($user) use ($router) {
+        $user['route'] = $router->urlFor('users.show', ['id' => $user['id']]);
+        return $user;
+    }, $filteredUsers);
+    
     $params['users'] = $filteredUsers;
     $params['term'] = $term;
+    $params['routeFormUserSearch'] = $router->urlFor('users.index');
     
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users.index');
 
-$app->post('/users', function ($request, $response) use ($users) {
+$app->post('/users', function ($request, $response) use ($users, $router) {
     $parsedUser = $request->getParsedBodyParam('user');
     $user = [
         'nickname' => htmlspecialchars($parsedUser['nickname']),
@@ -48,33 +57,30 @@ $app->post('/users', function ($request, $response) use ($users) {
         $users[] = $user;
         file_put_contents(__DIR__ . '/../users.json', json_encode($users));
 
-        return $response->withRedirect('/users', 302);        
+        return $response->withRedirect($router->urlFor('users.index'), 302);        
     }
     
     $params = [
         'errors' => $errors,
         'user' => $user,
+        'routeFormCreateUser' => $router->urlFor('users.index'),
     ];
     return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
-});
+})->setName('users.store');
 
 $app->get('/users/{id:[0-9]+}', function ($request, $response, $args) {
     $params = ['id' => htmlspecialchars($args['id']), 'nickname' => 'user-' . htmlspecialchars($args['id'])];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('users.show');
 
-$app->get('/users/new', function ($request, $response) {
+$app->get('/users/new', function ($request, $response) use ($router) {
     $params = [
         'user' => ['nickname' => '', 'email' => ''],
         'error' => [],
+        'routeFormCreateUser' => $router->urlFor('users.index'),
     ];
     
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
-});
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
+})->setName('users.create');
 
 $app->run();
